0b6c1885c7dc9fec24a5e5ec1a43f7cbd1ca8e9e
federico siviero <federico.siviero@edu.unito.it>
2025-02-26 17:42:27 +0100

    commit after TB7

---
+++ mkStat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+root -l Compilatore.C++
+cp beta_config.txt beta_config.ini
+root -l Compilatore.C++
---
+++ mkStat2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 2h 20m
+cp beta_config_2.txt beta_config.ini
+root -l Compilatore.C++
+cp beta_config_3.txt beta_config.ini
+root -l Compilatore.C++
